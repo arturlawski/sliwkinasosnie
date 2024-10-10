b5343946fd86f4037c945b47d9d91a022743d664
arturlawski <a.lawski@interia.pl>
2024-10-10 02:57:11 +0200

    fix7

--- src/components/Cart/CartItem/CartItemList.tsx
+++ src/components/Cart/CartItem/CartItemList.tsx
@@ -1,9 +1,9 @@
 import { AnimatePresence } from "framer-motion";
 // My import.
 import styles from "./CartItemList.module.css";
-import type DrinkItem from "models/DrinkItem";
 // My component.
 import CartItem from "./CartItem";
+import DrinkItem from "src/models/DrinkItem";
 
 type CartItemListProps = {
   items: DrinkItem[];
